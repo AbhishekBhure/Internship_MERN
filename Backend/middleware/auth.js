77285b4d4aa6186161eb60d18a03081a4ad417e6
AbhishekBhure <abhiab7411@gmail.com>
2022-11-02 01:33:01 -0700

    codee

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -6,7 +6,6 @@ const req = require("express/lib/request");
 
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   const token = req.cookies;
-  console.log(token);
   if (!token) {
     return next(new ErrorHandler("Please Login to access this resource", 401));
   }
